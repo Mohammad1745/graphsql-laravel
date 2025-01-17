604cf3b4c0ea7d0d169e3c895b189324fbe09803
Mohammad1745 <mdali2016.227@gmail.com>
2025-01-17 16:42:01 +0600

    [Add] nodes with table name

--- src/Schema.php
+++ src/Schema.php
@@ -13,11 +13,12 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 trait Schema
 {
     /**
-     * @param $modelPath
+     * @param string $modelPath
+     * @param string $search
      * @return array
      * @throws \ReflectionException
      */
-    public function getSchema ($modelPath='app/Models'): array
+    public function getSchema ($modelPath='app/Models', string $search=''): array
     {
 
         $files = File::allFiles( app_path( str_replace('app/', '', $modelPath)));
@@ -26,25 +27,29 @@ trait Schema
         foreach ($files as $file) {
             $namespace = app()->getNamespace();
             $className = $namespace . str_replace(
-                ['/', '.php'],
-                ['\\', ''],
-                Str::after($file->getPathname(), app_path() . DIRECTORY_SEPARATOR)
-            );
+                    ['/', '.php'],
+                    ['\\', ''],
+                    Str::after($file->getPathname(), app_path() . DIRECTORY_SEPARATOR)
+                );
             if (is_subclass_of($className, 'Illuminate\Database\Eloquent\Model') && !(new \ReflectionClass($className))->isAbstract()) {
                 $model = App::make($className);
 
+                $tableName = $model->getTable();
                 $fillable = $model->getFillable();
                 $hidden = $model->getHidden();
                 if (count($hidden)) {
                     $fillable = array_values( array_filter($fillable, fn($value) => !in_array($value, $hidden)));
                 }
 
-                $models[] = [
-                    'table' => $model->getTable(),
-                    'fields' => $fillable,
-                    'specialFields' => ['*', '_timestamps'],
-                    'nodes' => $this->getModelRelations($model)
-                ];
+                // search
+                if (str_starts_with($tableName, $search)) {
+                    $models[] = [
+                        'table' => $tableName,
+                        'fields' => $fillable,
+                        'specialFields' => ['*', '_timestamps'],
+                        'nodes' => $this->getModelRelations($model)
+                    ];
+                }
             }
         }
 
@@ -59,7 +64,25 @@ trait Schema
     {
         $methods = get_class_methods($model);
         $index = array_search('__construct', $methods);
+        $relations = $index === false ? [] : array_slice($methods, 0, $index);
 
-        return $index === false ? [] : array_slice($methods, 0, $index);
+        $relations = array_map( function ($node) use ($model) {
+            $relation = $model->{$node}();
+            $classPath = get_class($relation);
+            $relationName = str_replace("Illuminate\Database\Eloquent\Relations\\", '', $classPath);
+
+            $related = $relation->getRelated()->getTable();
+
+            if (strpos($classPath, 'BelongsToMany')) {
+                $pivot = $relation->getTable();
+                return "[$node]$pivot,$related";
+            }
+            else {
+                return "[$node]$related";
+            }
+
+        }, $relations);
+
+        return $relations;
     }
 }
